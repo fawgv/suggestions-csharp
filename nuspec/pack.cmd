a30d3af7dc0ea90a9f062b5b919bc5009ce002bf
Sergey Zhizhenko <zlobnyiserg@gmail.com>
2018-07-05 18:03:24 +0300

    Обновлена версия RestSharp

--- nuspec/pack.cmd
+++ nuspec/pack.cmd
@@ -1,2 +1,2 @@
-"../.nuget/nuget.exe" pack DaData.Client.nuspec -BasePath ../output
-"../.nuget/nuget.exe" push DaData.Client*.nupkg
+"..\.nuget\nuget.exe" pack DaData.Client.nuspec -BasePath ../output
+"..\.nuget\nuget.exe" push DaData.Client*.nupkg
